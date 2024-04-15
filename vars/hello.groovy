fada2fc5acf0cce6c1cf55ae88f10bc77bb98cf8
kariyaniamit <72973799+kariyaniamit@users.noreply.github.com>
2024-04-15 10:17:57 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,1 +1,28 @@
+pipeline {
+    agent any
 
+    stages {
+        stage('Build & Tag Docker Image') {
+            steps {
+                script {
+                    dir('src') {
+
+                    withDockerRegistry(credentialsId: 'docker-cred', toolName: 'docker') {
+                        sh "docker build -t aak11/cartservice:latest ."
+                    }
+                        }
+                }
+            }
+        }
+        
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    withDockerRegistry(credentialsId: 'docker-cred', toolName: 'docker') {
+                        sh "docker push aak11/cartservice:latest "
+                    }
+                }
+            }
+        }
+    }
+}
