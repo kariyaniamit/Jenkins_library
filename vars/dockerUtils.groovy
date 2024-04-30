c6bd071d5e349ae0a59f794342df19de6055aedd
kariyaniamit <72973799+kariyaniamit@users.noreply.github.com>
2024-04-30 16:07:19 +0530

    Update dockerUtils.groovy

--- vars/dockerUtils.groovy
+++ vars/dockerUtils.groovy
@@ -1,8 +1,24 @@
 // vars/dockerUtils.groovy
 def buildImage(String imageName, String imageTag) {
-    sh "docker build -t ${imageName}:${imageTag} ."
+    stage('Build & Tag Docker Image') {
+                steps {
+                    script {
+                        withDockerRegistry(credentialsId: 'docker_cred', toolName: 'docker') {
+                            sh "docker build -t ${imageName}:${imageTag} ."
+                        }
+                    }
+                }
+            }
 }
 
 def pushImage(String imageName, String imageTag) {
-    sh "docker build -t ${imageName}:${imageTag} ."
+    stage('Push Docker Image') {
+            steps {
+                script {
+                    withDockerRegistry(credentialsId: 'docker_cred', toolName: 'docker') {
+                        sh "docker push ${imageName}:${imageTag} "
+                    }
+                }
+            }
+        }  
 }
